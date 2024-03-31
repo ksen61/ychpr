c13e7e0d7786b32b527f51b72eeab90e6b4c10a8
ksen61 <145546631+ksen61@users.noreply.github.com>
2024-03-31 04:28:17 +0300

    Add files via upload

---
+++ ef/WarehouseWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace practica1ef
+{
+    /// <summary>
+    /// Логика взаимодействия для WarehouseWindow.xaml
+    /// </summary>
+    public partial class WarehouseWindow : Window
+    {
+        private PetShopEntities storehouse = new PetShopEntities();
+        public WarehouseWindow()
+        {
+            InitializeComponent();
+            gridStorehouse.ItemsSource = storehouse.Warehouse.ToList();
+
+        }
+    }
+}
